20a24b4e9188ba45a0a93badef433a6396b8eda6
M. Fatih Cırıt <mfcirit@gmail.com>
2021-02-05 15:26:47 +0300

    stamp

--- velodyne_pointcloud/src/conversions/convert.cc
+++ velodyne_pointcloud/src/conversions/convert.cc
@@ -67,8 +67,8 @@ namespace velodyne_pointcloud {
     //   outMsg(new velodyne_rawdata::XYZIRBPointCloud());
     // outMsg's header is a pcl::PCLHeader, convert it before stamp assignment
 
-    //outMsg->header.stamp = pcl_conversions::toPCL(scanMsg->header).stamp;
-    outMsg->header.stamp = pcl_conversions::toPCL(ros::Time::now());
+    outMsg->header.stamp = pcl_conversions::toPCL(scanMsg->header).stamp;
+//    outMsg->header.stamp = pcl_conversions::toPCL(ros::Time::now());
     outMsg->header.frame_id = scanMsg->header.frame_id;
     outMsg->height = 1;
 
